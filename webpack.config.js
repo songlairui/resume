0ec2e18159ca03ce9284e3302bcc2077402da195
songlairui <songlairui@126.com>
2019-12-23 17:09:32 +0800

    chore(reinit): scaffold
    use webpack@4

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,26 @@
-const path = require('path');
-const { WebPlugin } = require('web-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+const AssetConfigWebpackPlugin = require("asset-config-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const JsConfigWebpackPlugin = require("js-config-webpack-plugin");
+const ScssConfigWebpackPlugin = require("scss-config-webpack-plugin");
 
 module.exports = {
-  output: {
-    publicPath: '',
-    filename: '[name].js',
-  },
-  resolve: {
-    // 加快搜索速度
-    modules: [path.resolve(__dirname, 'node_modules')],
-    // es tree-shaking
-    mainFields: ['jsnext:main', 'browser', 'main'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        // 提取出css
-        loaders: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        }),
-        include: path.resolve(__dirname, 'src')
-      },
-      {
-        test: /\.css$/,
-        // 提取出css
-        loaders: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader'],
-        }),
-      },
-      {
-        test: /\.(gif|png|jpe?g|eot|woff|ttf|svg|pdf)$/,
-        loader: 'base64-inline-loader',
-      },
-    ]
-  },
-  entry: {
-    main: './src/main.js',
-  },
   plugins: [
-    new WebPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
+    // File loader configuration for .woff and .woff2 files
+    // File loader configuration for .gif .jpg .jpeg .png and .svg files
+    // https://github.com/namics/webpack-config-plugins/tree/master/packages/asset-config-webpack-plugin
+    new AssetConfigWebpackPlugin(),
+    // Cleans the dist folder before the build starts
+    new CleanWebpackPlugin(),
+    // Generate a base html file and injects all generated css and js files
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
     }),
-    new ExtractTextPlugin({
-      filename: '[name].css',
-      allChunks: true,
-    }),
-  ],
-  devtool: 'source-map',
+    // Multi threading babel loader configuration with caching for .js and .jsx files
+    // see https://github.com/namics/webpack-config-plugins/tree/master/packages/js-config-webpack-plugin/config
+    new JsConfigWebpackPlugin(),
+    // SCSS Configuration for .css .module.css and .scss .module.scss files
+    // see https://github.com/namics/webpack-config-plugins/tree/master/packages/scss-config-webpack-plugin/config
+    new ScssConfigWebpackPlugin()
+  ]
 };
